ed783d833b8e06fec1a2cf204d96b35c2326d2cb
sai ganesh <SAIGANESH.DHATRIKA@GMAIL.COM>
2018-09-02 22:50:44 -0400

    Add project files.

---
+++ Meetup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Meetup.Startup))]
+namespace Meetup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
